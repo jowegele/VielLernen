4a929fd7e7df54b2cef495d7d2cc04b4e6a97eda
Johannes Wegele <jowegele@gmail.com>
2017-11-05 14:38:43 +0100

    first commit

---
+++ src/VielLernen/Main.java
@@ -0,0 +1,41 @@
+package VielLernen;
+
+class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World \n");
+        Point lowerLeft = new Point();
+        Point lowerRight = new Point();
+
+        lowerLeft.x = 243.34;
+        lowerLeft.y = 32;
+
+        System.out.println("lowerLeft x value is " + lowerLeft.x);
+
+        lowerRight.x = 34;
+        lowerRight.y = 52.2;
+
+        System.out.println("\nlowerRight.x is " + lowerRight.x + " and lowerRight.y is " + lowerRight.y);
+        lowerRight.interchange();
+        System.out.println("\nlowerRight.x is " + lowerRight.x + " and lowerRight.y is " + lowerRight.y);
+             lowerRight.MultiplyByTwo();
+        System.out.println("\nlowerRight.x is " + lowerRight.x + " and lowerRight.y is " + lowerRight.y);
+
+        double d;
+        d = lowerRight.distance(lowerLeft);
+        System.out.println("\ndistance between lowerRight & lowerLeft: " + d);
+
+        d = lowerRight.Vektorprodukt();
+        System.out.println("\nVektorprodukt of lowerRight: " + d);
+
+        lowerLeft.clear();
+        System.out.println("\nlowerLeft x value after applying the methode clear() is " + lowerLeft.x);
+
+        lowerRight.clean();
+        System.out.println("\nlowerRight x value after applying the methode clean(): " + lowerRight.x);
+
+        System.out.println("\norigin is a static field: " + Point.origin.x +" / " + Point.origin.y);
+    }
+}
+
---
+++ src/VielLernen/Point.java
@@ -0,0 +1,38 @@
+package VielLernen;
+
+class Point {
+    private double z = 0.0;
+    double x, y;
+    void clear(){
+        x = z;
+        y = z;
+    }
+    void clean(){
+        x = z;
+        y = z;
+    }
+    void MultiplyByTwo(){
+        x = x*2;
+        y = y*2;
+    }
+    void interchange(){
+        x=y+x;
+        y=x-y;
+        x=x-y;
+    }
+
+    static Point origin;
+    static {
+        origin = new Point();
+    }
+    double Vektorprodukt(){
+        double xdiff = x;
+        double ydiff = y;
+        return Math.sqrt(xdiff * xdiff + ydiff + ydiff);
+    }
+    double distance(Point that){
+        double xdiff = x - that.x;
+        double ydiff = y - that.y;
+        return Math.sqrt(xdiff * xdiff + ydiff * ydiff);
+    }
+}
